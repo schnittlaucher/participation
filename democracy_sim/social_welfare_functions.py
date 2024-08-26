c84807a2ac020a219cdddadb9a0ae9cc572dd731
jurikane <jurikane@gmail.com>
2024-08-26 17:57:43 +0200

    Completed implementing majority rule including unit tests

--- democracy_sim/social_welfare_functions.py
+++ democracy_sim/social_welfare_functions.py
@@ -24,29 +24,57 @@ def complete_ranking(ranking: np.array, num_options: int):
     np.random.shuffle(non_included_options)
     return np.concatenate((ranking, non_included_options))
 
+def run_tie_breaking_preparation_for_majority(pref_table, noise_factor=100):
+    """
+        This function prepares the preference table for majority rule such that
+        it handles ties in the voters' preferences.
+        Because majority rule cannot usually deal with ties.
+        The tie breaking is randomized to ensure anonymity and neutrality.
+        :param pref_table: The agent's preferences.
+        :param noise_factor: Influences the amount of noise to be added
+               to the preference table to break ties (higher is less noise).
+        :return: The preference table without any ties.
+    """
+    # Add some random noise to break ties (based on the variances)
+    variances = np.var(pref_table, axis=1)
+    # If variances are zero, all values are equal, then select a random option
+    mask = (variances == 0)
+    # Split
+    pref_tab_var_zero = pref_table[mask]
+    pref_tab_var_non_zero = pref_table[~mask]
+    n, m = pref_tab_var_non_zero.shape
 
-def majority_rule(pref_table, noise_factor=100):
+    # Set all values in the var_zero_part to zero and then add a random 1
+    pref_tab_var_zero.fill(0)
+    for i in range(pref_tab_var_zero.shape[0]):
+        rand_option = np.random.randint(0, m)
+        pref_tab_var_zero[i, rand_option] = 1
+    # On the non-zero part, add some noise to the values to break ties
+    non_zero_variances = variances[~mask]
+    # Generate noise based on the variances
+    noise_eps = non_zero_variances / noise_factor
+    noise = np.random.uniform(-noise_eps[:, np.newaxis],
+                              noise_eps[:, np.newaxis], (n, m))
+    # noise_eps[:, np.newaxis] reshapes noise_eps from shape (n,) to (n, 1)
+    pref_tab_var_non_zero += noise
+
+    # Put the parts back together
+    return np.concatenate((pref_tab_var_non_zero, pref_tab_var_zero))
+
+def majority_rule(pref_table):
     """
     This function implements the majority rule social welfare function.
     :param pref_table: The agent's preferences as a NumPy matrix
             containing the normalized ranking vectors of all agents.
-    :param noise_factor: Influences the amount of noise to be added
-           to the preference table to break ties (higher is less noise).
     :return: The resulting preference ranking (beware: its not a pref. relation)
     """
     n, m = pref_table.shape  # n agents, m options
-    # Add a tiny amount of random noise to break ties
-    # Beware: Even with noise added, argmax will have a bias for lower indices
-    # on arrays with too many ties. So this is just a small tiebreaker.
-    variances = np.var(pref_table, axis=0) + 1e-10
-    # Generate noise based on the variances to sensitively break ties
-    noise_eps = variances / noise_factor
-    noise = np.random.uniform(-noise_eps, noise_eps, (n, m))
-    pref_table += noise
+    # Break ties if they exist
+    pref_table = run_tie_breaking_preparation_for_majority(pref_table)
     # Count how often an option is ranked first (indexes of max values)
     first_choices = np.argmax(pref_table, axis=1)
-    # To avoid a bias toward voters with low indices in the counting, we shuffle
-    np.random.shuffle(first_choices)
+    # To avoid bias toward voters of low indices in the counting, we shuffle
+    np.random.shuffle(first_choices)  # (crucial when counting shows ties later)
     first_choice_counts = {}
     for choice in first_choices:
         first_choice_counts[choice] = first_choice_counts.get(choice, 0) + 1
@@ -64,25 +92,6 @@ def majority_rule(pref_table, noise_factor=100):
     if ranking.shape[0] < m:
         ranking = complete_ranking(ranking, m)
     return ranking
-
-
-#def majority_rule_2(pref_table):
-    # """
-    # This function implements the majority rule social welfare function.
-    # :param pref_table: The agent's preferences as a NumPy matrix
-    #         one row one agent, column number is color,
-    #         values are the guessed distribution values (not the ranking!)
-    # :return: The resulting preference ranking
-    # """
-
-    # # Count how often each color is guessed as
-    # first_choices = np.argmax(pref_table, axis=0)
-    # first_choice_counts = {}
-    # for choice in first_choices:
-    #     first_choice_counts[choice] = first_choice_counts.get(choice, 0) + 1
-    # option_count_pairs = list(first_choice_counts.items())
-    # option_count_pairs.sort(key=lambda x: x[1], reverse=True)
-    # return [pair[0] for pair in option_count_pairs]
 
 
 def approval_voting(pref_table):
---
+++ tests/test_majority_rule.py
@@ -0,0 +1,223 @@
+import numpy as np
+import time
+from democracy_sim.social_welfare_functions import majority_rule
+
+# Simple and standard cases
+
+simple = np.array([
+    [0.5, 0.4, 0.1],
+    [0.1, 0.5, 0.4],
+    [0.4, 0.5, 0.1],
+    [0.1, 0.4, 0.5],
+    [0.1, 0.4, 0.5],
+    [0.1, 0.4, 0.5]
+])  # => c, b, a ~ 2, 1, 0
+
+# Following "paradoxical" example is taken from
+# https://pub.dss.in.tum.de/brandt-research/minpara.pdf
+#
+#    5 4 3 2
+#    -------
+#    a e d b
+#    c b c d
+#    b c b e
+#    d d e c
+#    e a a a
+
+paradoxical = np.array([
+    # 5 times a,c,b,d,e --> 0.4, 0.2, 0.3, 0.1, 0.
+    [0.4, 0.2, 0.3, 0.1, 0. ],
+    [0.4, 0.2, 0.3, 0.1, 0. ],
+    [0.4, 0.2, 0.3, 0.1, 0. ],
+    [0.4, 0.2, 0.3, 0.1, 0. ],
+    [0.4, 0.2, 0.3, 0.1, 0. ],
+    # 4 times e,b,c,d,a
+    [0. , 0.3, 0.2, 0.1, 0.4],
+    [0. , 0.3, 0.2, 0.1, 0.4],
+    [0. , 0.3, 0.2, 0.1, 0.4],
+    [0. , 0.3, 0.2, 0.1, 0.4],
+    # 3 times d,c,b,e,a
+    [0. , 0.2, 0.3, 0.4, 0.1],
+    [0. , 0.2, 0.3, 0.4, 0.1],
+    [0. , 0.2, 0.3, 0.4, 0.1],
+    # 2 times b,d,e,c,a
+    [0. , 0.4, 0.1, 0.3, 0.2],
+    [0. , 0.4, 0.1, 0.3, 0.2]
+])  # Plurality => a, e, d, b, c ~ 0, 4, 3, 1, 2
+
+majority_simple_cases = [
+    (simple, [2, 1, 0]),
+    (paradoxical, [0, 4, 3, 1, 2])
+]
+
+def test_majority_rule():
+    # Test predefined cases
+    for pref_table, expected in majority_simple_cases:
+        res_ranking = majority_rule(pref_table)
+        assert list(res_ranking) == expected
+
+def majority_rule_with_ties_all(pref_rel, expected_winners, iterations=1000):
+    """
+    Run majority rule with ties multiple times, check winners
+    and calculate the coefficient of variation (CV) of the winners.
+    :param pref_rel: Preference relation matrix.
+    :param expected_winners: An ordered list of expected winners, i.e. [0, 1].
+    :param iterations: Number of iterations.
+    -------
+    :return: Coefficient of variation (CV) of the winners.
+    """
+    winners_from_ties = {}
+    for _ in range(iterations):
+        ranking = majority_rule(pref_rel)
+        winner = ranking[0]
+        winners_from_ties[winner] = winners_from_ties.get(winner, 0) + 1
+    winners = list(winners_from_ties.keys())
+    winners.sort()
+    assert winners == expected_winners
+    counts = np.array(list(winners_from_ties.values()))
+    # Calculate the coefficient of variation (CV)
+    cv = np.std(counts) / np.mean(counts)
+    return cv
+
+# Cases with ties - "all equally possible"
+
+with_ties_all = np.array([
+        [0.25, 0.25, 0.25, 0.25],
+        [0.25, 0.25, 0.25, 0.25],
+        [0.25, 0.25, 0.25, 0.25],
+        [0.25, 0.25, 0.25, 0.25],
+        [0.25, 0.25, 0.25, 0.25]
+    ])
+
+with_overall_tie = np.array([
+    [0.4, 0.3, 0.2, 0.1],
+    [0.1, 0.4, 0.3, 0.2],
+    [0.2, 0.1, 0.4, 0.3],
+    [0.3, 0.2, 0.1, 0.4],
+])
+
+with_ties_mixed = np.array([
+    [0.4, 0.3, 0.2, 0.1],
+    [0.25, 0.25, 0.25, 0.25],
+    [0.25, 0.25, 0.25, 0.25],
+    [0.1, 0.4, 0.3, 0.2],
+    [0.2, 0.1, 0.4, 0.3],
+    [0.25, 0.25, 0.25, 0.25],
+    [0.3, 0.2, 0.1, 0.4],
+])
+
+all_equally_possible = [with_ties_all, with_overall_tie, with_ties_mixed]
+
+def test_equally_possible(cv_threshold=0.125):
+    for pref_rel in all_equally_possible:
+        cv = majority_rule_with_ties_all(pref_rel, [0, 1, 2, 3])
+        print(f"CV: {cv}")
+        assert cv < cv_threshold
+
+# Cases with ties - "not all equally possible"
+with_ties_unequal = np.array([
+        [0.25, 0.25, 0.25, 0.25],
+        [0.4, 0.3, 0.2, 0.1],
+        [0.25, 0.25, 0.25, 0.25],
+        [0.25, 0.25, 0.25, 0.25],
+        [0.25, 0.25, 0.25, 0.25]
+    ])
+
+with_ties_all_ab = np.array([
+        [0.3, 0.3, 0.2, 0.2],
+        [0.25, 0.25, 0.25, 0.25]
+    ])  # all possible (a or b up first is more likely)
+
+with_ties_ab = np.array([
+        [0.3, 0.3, 0.2, 0.2],
+        [0.3, 0.3, 0.2, 0.2],
+        [0.25, 0.25, 0.25, 0.25]
+    ])  # all possible (a or b up first is more likely)
+
+with_ties_unequal = [with_ties_unequal, with_ties_all_ab, with_ties_ab]
+
+def test_with_ties_unequal():
+    for pref_rel in with_ties_unequal:
+        cv = majority_rule_with_ties_all(pref_rel, [0, 1, 2, 3])
+        print(f"CV: {cv}")
+        assert cv > 0.125
+
+# Random matrix
+
+def random_pref_profile(num_agents, num_options):
+    rand_matrix = np.random.rand(num_agents, num_options)
+    # Normalize the matrix
+    matrix_rand = rand_matrix / rand_matrix.sum(axis=1, keepdims=True)
+    return matrix_rand
+
+def majority_rule_with_rand_matrix(num_agents, num_options, iterations=1000):
+    """
+    Run majority rule with ties multiple times, check winners
+    and calculate the coefficient of variation (CV) of the winners.
+    :param num_agents: Number of agents.
+    :param num_options: Number of options.
+    :param iterations: Number of iterations.
+    -------
+    :return: Dictionary of winner counts {option: count}.
+    """
+    winner_counts = {}
+    for _ in range(iterations):
+        # Create random matrix
+        matrix_rand = random_pref_profile(num_agents, num_options)
+        ranking = majority_rule(matrix_rand)
+        winner = ranking[0]
+        # Count winners
+        winner_counts[winner] = winner_counts.get(winner, 0) + 1
+    return winner_counts
+
+
+def test_with_random_matrix_small():
+    """
+    Test majority rule on a small random matrix with many iterations.
+    """
+    num_agents = np.random.randint(2, 200)
+    # Keep num options small to expect all options to win at least once.
+    num_options = np.random.randint(2, 90)
+    iterations = 100*num_options
+    start_time = time.time()
+    wc = majority_rule_with_rand_matrix(num_agents, num_options, iterations)
+    stop_time = time.time()
+    # Extract winners from winner-counts dictionary and sort them
+    sorted_winners = list(wc.keys())
+    sorted_winners.sort()
+    assert sorted_winners == list(range(num_options))
+    # Extract count values
+    counts = np.array(list(wc.values()))
+    # Calculate the coefficient of variation (CV)
+    cv = np.std(counts) / np.mean(counts)
+    assert cv < 0.125
+    print(f"\nCV: {cv}")
+    # Print the time taken
+    elapsed_time = stop_time - start_time
+    print(f"\nTime taken: {elapsed_time:.2f} sec. On {iterations} iterations."
+          f"With {num_agents} agents and {num_options} options.")
+
+
+def test_with_random_matrix_large():
+    """
+    Test majority rule on a large random matrix (many agents, many options).
+    """
+    num_its = 100
+    num_agents = np.random.randint(1000, 3000)
+    num_options = np.random.randint(2000, 3000)
+    # Run majority rule test with random matrix
+    start_time = time.time()
+    wc = majority_rule_with_rand_matrix(num_agents, num_options, num_its)
+    stop_time = time.time()
+    # Len of winners should be approximately equal to the number of iterations
+    # because with a large number of options, winners should be mostly unique.
+    winners, counts = list(wc.keys()), list(wc.values())
+    assert abs(np.mean(counts) - 1) < 0.1
+    assert abs((len(winners) / num_its) - 1) < 0.1
+    # Calculate the coefficient of variation (CV)
+    cv = np.std(counts) / np.mean(counts)
+    assert cv < 0.2
+    # Print the time taken
+    elapsed_time = stop_time - start_time
+    print(f"\nTime taken: {elapsed_time:.2f} sec. On {num_its} iterations."
+          f"With {num_agents} agents and {num_options} options.")
